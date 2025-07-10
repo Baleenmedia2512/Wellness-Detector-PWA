6172028bdecc85533452ea389bcb051371988a61
Logesh6767 <logeshwaran67677@gmail.com>
2025-07-10 17:35:13 +0530

    For deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,6 @@ import Header from './components/Header';
 
 function App() {
   const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-  console.log(apiBaseUrl)
   const [selectedImage, setSelectedImage] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
   const [nutritionData, setNutritionData] = useState(null);
